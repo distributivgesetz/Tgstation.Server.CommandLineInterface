fedbe08b2bed56ef2466b32912aa15d08506c104
distributivgesetz <distributivgesetz93@gmail.com>
2023-10-12 15:28:58 +0200

    initial commit, hello world command line program

---
+++ Commands/HelloWorld.cs
@@ -0,0 +1,16 @@
+using CliFx;
+using CliFx.Attributes;
+using CliFx.Infrastructure;
+
+namespace Tgstation.Server.CommandLineInterface.Commands
+{
+    [Command("hello-world")]
+    public class HelloWorld : ICommand
+    {
+        public ValueTask ExecuteAsync(IConsole console)
+        {
+            console.Output.WriteLine("Hello, world!");
+            return default;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using CliFx;
+using Microsoft.Extensions.DependencyInjection;
+
+return await new CliApplicationBuilder()
+    .SetExecutableName("tgs-cli") // :3c
+    .UseTypeActivator(ConfigureServices)
+    .AddCommandsFromThisAssembly()
+    .Build()
+    .RunAsync();
+
+IServiceProvider ConfigureServices(IEnumerable<Type> commands)
+{
+    var services = new ServiceCollection();
+
+    foreach (var command in commands) 
+        services.AddTransient(command);
+
+    return services.BuildServiceProvider();
+}
