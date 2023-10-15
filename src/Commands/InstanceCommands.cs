f1fe06d27f844d819f203fbb80cdc63178da5a48
distributivgesetz <distributivgesetz93@gmail.com>
2023-10-15 14:45:39 +0200

    unfinished instances list command

---
+++ src/Commands/InstanceCommands.cs
@@ -0,0 +1,28 @@
+﻿namespace Tgstation.Server.CommandLineInterface.Commands;
+
+using CliFx;
+using CliFx.Attributes;
+using CliFx.Infrastructure;
+using Services;
+
+[Command("instances list")]
+public class ListInstancesCommand : ICommand
+{
+    private readonly IRemoteRegistry remotes;
+    private readonly ISessionManager sessions;
+
+    public ListInstancesCommand(IRemoteRegistry remotes, ISessionManager sessions)
+    {
+        this.remotes = remotes;
+        this.sessions = sessions;
+    }
+
+    public async ValueTask ExecuteAsync(IConsole console)
+    {
+        this.FailIfNoRemote(this.remotes);
+        await this.TryMakeRequest(async () =>
+        {
+            throw new NotImplementedException();
+        });
+    }
+}
