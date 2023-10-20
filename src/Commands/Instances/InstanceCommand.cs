5a052f22fef2f278053be553369114dfc92deae4
distributivgesetz <distributivgesetz93@gmail.com>
2023-10-20 10:57:47 +0200

    base instance command

---
+++ src/Commands/Instances/InstanceCommand.cs
@@ -0,0 +1,13 @@
+﻿namespace Tgstation.Server.CommandLineInterface.Commands.Instances;
+
+using CliFx;
+using CliFx.Attributes;
+using CliFx.Exceptions;
+using CliFx.Infrastructure;
+
+[Command("instance")]
+public class InstanceCommand : ICommand
+{
+    public ValueTask ExecuteAsync(IConsole console) =>
+        throw new CommandException("Please provide an option.", showHelp: true);
+}
