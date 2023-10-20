3004f49352ca187a48ec778c137b145d41001f54
distributivgesetz <distributivgesetz93@gmail.com>
2023-10-20 11:05:06 +0200

    program tweaks

--- src/Services/ApplicationInfo.cs
+++ src/Services/ApplicationInfo.cs
@@ -5,13 +5,14 @@ using System.Reflection;
 public interface IApplicationInfo
 {
     string Name { get; }
-    Version Version { get; }
+    string Version { get; }
 }
 
 public sealed class ApplicationInfo : IApplicationInfo
 {
     public const string ApplicationName = "TGS-CLI";
+    public static readonly string ApplicationVersion = Assembly.GetExecutingAssembly().GetName().Version!.ToString();
 
     public string Name => ApplicationName;
-    public Version Version { get; } = Assembly.GetExecutingAssembly().GetName().Version!;
+    public string Version => ApplicationVersion;
 }
