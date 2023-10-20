cfd4f117392e03ae5db1e5d194908a1035ae9202
distributivgesetz <distributivgesetz93@gmail.com>
2023-10-20 10:25:00 +0200

    properly catch socket exceptions

--- src/Middlewares/Implementations/RequestFailHandlerMiddleware.cs
+++ src/Middlewares/Implementations/RequestFailHandlerMiddleware.cs
@@ -3,24 +3,29 @@ namespace Tgstation.Server.CommandLineInterface.Middlewares.Implementations;
 using System.Net.Sockets;
 using Client;
 using CliFx.Exceptions;
+using Services;
 
 public class RequestFailHandlerMiddleware : ICommandMiddleware
 {
+    private readonly IRemoteRegistry remotes;
+
+    public RequestFailHandlerMiddleware(IRemoteRegistry remotes) => this.remotes = remotes;
+
     public async ValueTask HandleCommandAsync(IMiddlewareContext context, PipelineNext nextStep)
     {
+        var host = this.remotes.GetCurrentRemote().Host;
+
         try
         {
             await nextStep();
         }
         catch (ApiException e)
         {
-            throw new CommandException($"API returned an error: {e.ErrorCode.ToString()}");
+            throw new CommandException($"{host} returned an error! Code is {e.ErrorCode.ToString()}");
         }
-        catch (HttpRequestException e) when (e.InnerException?
-                                                 .GetType().IsSubclassOf(typeof(SocketException))
-                                             ?? false)
+        catch (HttpRequestException e) when (e.InnerException is SocketException)
         {
-            throw new CommandException($"Request to API failed: {e.InnerException.Message}");
+            throw new CommandException($"Request to {host} failed! Reason is {e.InnerException.Message}");
         }
     }
 }
