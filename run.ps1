752aba0e059c15d6de950f709ebcae7a23383dc4
distributivgesetz <distributivgesetz93@gmail.com>
2023-10-25 16:58:39 +0200

    run.ps1 restores cwd correctly

--- run.ps1
+++ run.ps1
@@ -1,2 +1,7 @@
-Set-Location user
-dotnet run --project ../src/tgs-cli.csproj -- $args
+Push-Location -Path "./user"
+try {
+    dotnet run --project ../src/tgs-cli.csproj -- $args
+}
+finally {
+    Pop-Location
+}
