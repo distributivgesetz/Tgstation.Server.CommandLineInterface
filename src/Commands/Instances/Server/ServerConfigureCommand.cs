b79ab6acb0b425e557782c09198ef0db27ccb827
distributivgesetz <distributivgesetz93@gmail.com>
2023-10-31 13:43:52 +0100

    server configure stub command

---
+++ src/Commands/Instances/Server/ServerConfigureCommand.cs
@@ -0,0 +1,12 @@
+﻿namespace Tgstation.Server.CommandLineInterface.Commands.Instances.Server;
+
+using CliFx.Attributes;
+using Middlewares;
+using Sessions;
+
+[Command("instance server configure")]
+public class ServerConfigureCommand : BaseSessionCommand
+{
+    protected override ValueTask RunCommandAsync(ICommandContext context) =>
+        throw new NotImplementedException("This command is not yet implemented"); // TODO
+}
