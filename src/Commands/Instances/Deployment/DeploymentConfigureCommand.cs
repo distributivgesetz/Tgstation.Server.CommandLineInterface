0a4661eaffed5d5b2b7e5ca65728f2628e25107c
distributivgesetz <distributivgesetz93@gmail.com>
2023-10-31 13:25:23 +0100

    deployment settings configure command

---
+++ src/Commands/Instances/Deployment/DeploymentConfigureCommand.cs
@@ -0,0 +1,45 @@
+﻿namespace Tgstation.Server.CommandLineInterface.Commands.Instances.Deployment;
+
+using Api.Models;
+using Api.Models.Request;
+using CliFx.Attributes;
+using Middlewares;
+using Models;
+using Services;
+using Sessions;
+
+[Command("instance deploy configure")]
+public class DeploymentConfigureCommand : BaseSessionCommand
+{
+    private readonly IInstanceManager instances;
+
+    public DeploymentConfigureCommand(IInstanceManager instances) => this.instances = instances;
+
+    [CommandParameter(0, Converter = typeof(InstanceSelectorConverter))]
+    public required InstanceSelector Instance { get; init; }
+
+    [CommandOption("project", 'n')] public string? ProjectName { get; init; }
+
+    [CommandOption("port", 'p')] public ushort? ValidationPort { get; init; }
+
+    [CommandOption("level", 's', Converter = typeof(DreamDaemonSecurityConverter))]
+    public DreamDaemonSecurity? SecurityLevel { get; init; }
+
+    [CommandOption("validate", 'v')] public bool? ValidationRequired { get; init; }
+
+    [CommandOption("timeout", 't')] public TimeSpan? Timeout { get; init; }
+
+    protected override async ValueTask RunCommandAsync(ICommandContext context)
+    {
+        var client = await this.instances.RequestInstanceClient(this.Instance, context.CancellationToken);
+        await client.DreamMaker.Update(
+            new DreamMakerRequest
+            {
+                ProjectName = this.ProjectName,
+                ApiValidationPort = this.ValidationPort,
+                ApiValidationSecurityLevel = this.SecurityLevel,
+                RequireDMApiValidation = this.ValidationRequired,
+                Timeout = this.Timeout
+            }, context.CancellationToken);
+    }
+}
---
+++ src/Models/DreamDaemonSecurityConverter.cs
@@ -0,0 +1,10 @@
+﻿namespace Tgstation.Server.CommandLineInterface.Models;
+
+using Api.Models;
+using CliFx.Extensibility;
+
+public class DreamDaemonSecurityConverter : BindingConverter<DreamDaemonSecurity>
+{
+    public override DreamDaemonSecurity Convert(string? rawValue) =>
+        Enum.TryParse<DreamDaemonSecurity>(rawValue, out var res) ? res : throw new FormatException();
+}
