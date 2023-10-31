94fcd8911dccb455c6c800c527bf8681e01966b5
distributivgesetz <distributivgesetz93@gmail.com>
2023-10-31 11:30:07 +0100

    better engine version command

--- src/Commands/Instances/Engine/GetEngineVersionCommand.cs
+++ src/Commands/Instances/Engine/GetEngineVersionCommand.cs
@@ -1,5 +1,7 @@
 ﻿namespace Tgstation.Server.CommandLineInterface.Commands.Instances.Engine;
 
+using System.Globalization;
+using System.Text;
 using CliFx.Attributes;
 using Extensions;
 using Middlewares;
@@ -20,7 +22,16 @@ public class GetEngineVersionCommand : BaseSessionCommand
     protected override async ValueTask RunCommandAsync(ICommandContext context)
     {
         var client = await this.instances.RequestInstanceClient(this.Instance, context.CancellationToken);
-        var response = await client.Byond.ActiveVersion(context.CancellationToken);
-        await context.Console.WriteLineAsync(response.Version!.ToString());
+        var currentVersion = await client.Byond.ActiveVersion(context.CancellationToken);
+        var availableVersions = await client.Byond.InstalledVersions(null, context.CancellationToken);
+
+        var output = new StringBuilder();
+        output.AppendLine(CultureInfo.InvariantCulture, $"Current version: {currentVersion.Version!}");
+        output.AppendLine("Available versions:");
+        foreach (var version in availableVersions)
+        {
+            output.AppendLine(version.Version!.ToString());
+        }
+        await context.Console.WriteAsync(output.ToString());
     }
 }
