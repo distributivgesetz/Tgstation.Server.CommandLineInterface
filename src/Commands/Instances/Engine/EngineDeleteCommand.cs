26ce798ca2804f2f91a63ac886d62572c4481b89
distributivgesetz <distributivgesetz93@gmail.com>
2023-10-31 11:30:25 +0100

    engine version delete command

---
+++ src/Commands/Instances/Engine/EngineDeleteCommand.cs
@@ -0,0 +1,29 @@
+﻿namespace Tgstation.Server.CommandLineInterface.Commands.Instances.Engine;
+
+using Api.Models.Request;
+using CliFx.Attributes;
+using Middlewares;
+using Models;
+using Services;
+using Sessions;
+
+[Command("instance engine delete")]
+public class EngineDeleteCommand : BaseSessionCommand
+{
+    private readonly IInstanceManager instances;
+
+    public EngineDeleteCommand(IInstanceManager instances) => this.instances = instances;
+
+    [CommandParameter(0, Converter = typeof(InstanceSelectorConverter))]
+    public required InstanceSelector Instance { get; set; }
+
+    [CommandParameter(1, Converter = typeof(VersionConverter))]
+    public required Version Version { get; set; }
+
+    protected override async ValueTask RunCommandAsync(ICommandContext context)
+    {
+        var client = await this.instances.RequestInstanceClient(this.Instance, context.CancellationToken);
+        await client.Byond.DeleteVersion(new ByondVersionDeleteRequest() {Version = this.Version}, context.CancellationToken);
+    }
+
+}
