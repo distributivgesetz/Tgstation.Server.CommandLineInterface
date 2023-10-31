c047c1f0eef49a2c8c061673763689cc39bbe3ed
distributivgesetz <distributivgesetz93@gmail.com>
2023-10-31 13:57:07 +0100

    users list command

---
+++ src/Commands/Users/UsersListCommand.cs
@@ -0,0 +1,32 @@
+﻿namespace Tgstation.Server.CommandLineInterface.Commands.Users;
+
+using System.Globalization;
+using System.Text;
+using CliFx.Attributes;
+using Extensions;
+using Middlewares;
+using Services;
+using Sessions;
+
+[Command("user list")]
+public class UsersListCommand : BaseSessionCommand
+{
+    private readonly ISessionManager sessions;
+
+    public UsersListCommand(ISessionManager sessions) => this.sessions = sessions;
+
+    protected override async ValueTask RunCommandAsync(ICommandContext context)
+    {
+        var client = await this.sessions.ResumeSession(context.CancellationToken);
+        var users = await client.Users.List(null, context.CancellationToken);
+
+        var output = new StringBuilder();
+
+        foreach (var user in users)
+        {
+            output.AppendLine(CultureInfo.InvariantCulture, $"{user.Name} ({user.Id})");
+        }
+
+        await context.Console.WriteAsync(output.ToString());
+    }
+}
