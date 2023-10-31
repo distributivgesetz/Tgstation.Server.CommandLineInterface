f92d8314409a733d5d27b282f68e3b8876813f9b
distributivgesetz <distributivgesetz93@gmail.com>
2023-10-31 13:24:49 +0100

    create deployment command

---
+++ src/Commands/Instances/Deployment/CreateDeploymentCommand.cs
@@ -0,0 +1,24 @@
+﻿namespace Tgstation.Server.CommandLineInterface.Commands.Instances.Deployment;
+
+using CliFx.Attributes;
+using Middlewares;
+using Models;
+using Services;
+using Sessions;
+
+[Command("instance deploy compile")]
+public class CreateDeploymentCommand : BaseSessionCommand
+{
+    private readonly IInstanceManager instances;
+
+    public CreateDeploymentCommand(IInstanceManager instances) => this.instances = instances;
+
+    [CommandParameter(0, Converter = typeof(InstanceSelectorConverter))]
+    public required InstanceSelector Instance { get; init; }
+
+    protected override async ValueTask RunCommandAsync(ICommandContext context)
+    {
+        var client = await this.instances.RequestInstanceClient(this.Instance, context.CancellationToken);
+        await client.DreamMaker.Compile(context.CancellationToken);
+    }
+}
