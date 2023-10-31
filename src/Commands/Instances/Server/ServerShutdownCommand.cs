2fb8f910ffd45c7ac9cd1f34c31b39e948a954b8
distributivgesetz <distributivgesetz93@gmail.com>
2023-10-31 13:35:38 +0100

    server startup and shutdown commands

---
+++ src/Commands/Instances/Server/ServerShutdownCommand.cs
@@ -0,0 +1,24 @@
+﻿namespace Tgstation.Server.CommandLineInterface.Commands.Instances.Server;
+
+using CliFx.Attributes;
+using Middlewares;
+using Models;
+using Services;
+using Sessions;
+
+[Command("instance server shutdown")]
+public class ServerShutdownCommand : BaseSessionCommand
+{
+    private readonly IInstanceManager instances;
+
+    public ServerShutdownCommand(IInstanceManager instances) => this.instances = instances;
+
+    [CommandParameter(0, Converter = typeof(InstanceSelectorConverter))]
+    public required InstanceSelector Instance { get; init; }
+
+    protected override async ValueTask RunCommandAsync(ICommandContext context)
+    {
+        var client = await this.instances.RequestInstanceClient(this.Instance, context.CancellationToken);
+        await client.DreamDaemon.Shutdown(context.CancellationToken);
+    }
+}
